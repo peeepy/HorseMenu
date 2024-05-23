99c7becb7437e113b12e544c4fa0b15efae38822
peeepy <111094949+peeepy@users.noreply.github.com>
2024-05-23 17:21:19 +0100

    fix ped.hpp to take 9 arguments

--- src/util/Ped.hpp
+++ src/util/Ped.hpp
@@ -7,5 +7,5 @@
 
 namespace YimMenu::Peds
 {
-	extern int SpawnPed(std::string model_name, Vector3 coords, float heading = 0.0f, bool blockNewPedMovement = false, bool spawnDead = false, bool invincible = false, bool invisible = false, int scale = 1);
+	extern int SpawnPed(std::string model_name, Vector3 coords, float heading = 0.0f, bool blockNewPedMovement = false, bool spawnDead = false, bool invincible = false, bool invisible = false, int scale = 1, bool companionMode = false);
 }
